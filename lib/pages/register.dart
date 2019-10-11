b3717e0a614219b01e423ce2750453f6f5419757
ScarponeO <orlando.scarpone@correo.unimet.edu.ve>
2019-10-11 15:43:51 -0400

    Register y Login

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -23,15 +23,41 @@ class _RegisterPageState extends State<RegisterPage> {
           padding: EdgeInsets.all(20.0),
           child: ScrollConfiguration(
             behavior: HiddenScrollBehavior(),
-            child: ListView(
-              children: <Widget>[
-                FlutterLogo(
-                  style: FlutterLogoStyle.markOnly ,
-                size: 200.0,)
-              ],
+            child: Form(
+              child:  ListView(
+                children: <Widget>[
+                  FlutterLogo(
+                    style: FlutterLogoStyle.markOnly ,
+                    size: 200.0,
+                  ),
+                  TextFormField(
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress ,
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Password'),
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: Text(
+                      'Welcome' ,style: TextStyle(color: Color.fromARGB(255, 200, 200, 200))),
+                  ) ,
+
+                ],
+              ),
             ) ,
           )
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){} ,
+        child: Icon(Icons.person_add),
+      ) ,
+      persistentFooterButtons: <Widget>[
+        FlatButton(onPressed: (){
+          Navigator.of(context).pushNamed('/login');
+        }, child: Text('Ya tengo una cuenta') ,)
+      ],
     );
   }
 
